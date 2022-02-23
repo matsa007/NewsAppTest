5683810837899c060cadf2694113c1ef8dd26406
matsa_007 <serg.arbitran@gmail.com>
2022-02-23 17:46:17 +0300

    no message

---
+++ NewsTestApp/Models/NewsTableViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  NewsTableViewModel.swift
+//  NewsTestApp
+//
+//  Created by Сергей Матвеенко on 23.02.2022.
+//
+
+import Foundation
+
+final class NewsTableViewCellModel {
+    let title: String
+    let subTitle: String
+    let imageUrl: URL?
+    let contentUrl: String
+    let publishedAt: String
+    var imageData: Data? = nil
+    
+    init(title: String, subTitle: String, imageUrl: URL, contentUrl: String, publishedAt: String) {
+        self.title = title
+        self.subTitle = subTitle
+        self.imageUrl = imageUrl
+        self.contentUrl = contentUrl
+        self.publishedAt = publishedAt
+    }
+}
